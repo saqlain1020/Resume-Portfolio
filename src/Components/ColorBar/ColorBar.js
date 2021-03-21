4d197061dca003cbdd072049fa9654a27cb4d901
Saqlain <saqlainprinters@gmail.com>
2021-03-21 22:08:07 +0500

    Menu Icon animate

--- src/Components/ColorBar/ColorBar.js
+++ src/Components/ColorBar/ColorBar.js
@@ -6,6 +6,7 @@ import { IconButton } from "@material-ui/core";
 import MenuRoundedIcon from "@material-ui/icons/MenuRounded";
 import { closeMenu, openMenu } from "src/Redux/menu/menuReducer";
 import CloseRoundedIcon from "@material-ui/icons/CloseRounded";
+import { motion } from "framer-motion";
 
 const ColorBar = ({ setTheme, menu, openMenu, closeMenu }) => {
   const classes = useStyles();
@@ -13,13 +14,29 @@ const ColorBar = ({ setTheme, menu, openMenu, closeMenu }) => {
   return (
     <div className={classes.root}>
       <div className={classes.colorMenu}>
-        <IconButton>
-          {menu ? (
-            <CloseRoundedIcon onClick={closeMenu} />
+        <motion.div
+          whileTap={{
+            scale: 0.8,
+            rotate: -90,
+          }}
+          initial={{ scale: 0 }}
+          animate={{ rotate: 180, scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 260,
+            damping: 20,
+          }}
+        >
+          {!menu ? (
+            <IconButton onClick={openMenu}>
+              <MenuRoundedIcon />
+            </IconButton>
           ) : (
-            <MenuRoundedIcon onClick={openMenu} />
+            <IconButton onClick={closeMenu}>
+              <CloseRoundedIcon />
+            </IconButton>
           )}
-        </IconButton>
+        </motion.div>
       </div>
       <div
         className={classes.color}
